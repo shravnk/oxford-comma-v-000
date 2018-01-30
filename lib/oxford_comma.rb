c13dba055d08c42b35a186ba41b874e23caa5b27
Brendan O'Dea <brendan.ode4@gmail.com>
2018-01-30 05:08:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  oxford = ""
+  array.each_with_index do |item, index|
+    if index == array.size - 1
+      oxford << item
+    else
+      oxford << item 
+      oxford << ", "
+    end
+  end
 end
