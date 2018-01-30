d7d65f3529ac88c6aaec6307fe0c51a9e264848a
Brendan O'Dea <brendan.ode4@gmail.com>
2018-01-30 05:15:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,12 @@
 def oxford_comma(array)
   oxford = ""
   array.each_with_index do |item, index|
-    if index == array.size - 1
+    if index == array.size - 1 && array.size != 1
       oxford.concat("and ")
       oxford.concat(item)
+    elsif index == array.size -2 &&
+      oxford.concat(item)
+      oxford.concat(" ")
     else
       oxford.concat(item)
       oxford.concat(", ")
